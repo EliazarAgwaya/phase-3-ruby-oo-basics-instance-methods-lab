cca50f1770c8b47c150156c05f5413f0c05f57df
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-08-17 09:30:20 +0300

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+teddy = Person.new
+teddy.talk
+teddy.walk
+
+eliazar = Person.new
+eliazar.talk
+eliazar.walk
